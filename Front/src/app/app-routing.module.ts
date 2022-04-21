8224be248e3239dc66615694b87834cf6edb0456
sylvain <yvonneau.sylvain@gmail.com>
2022-04-21 15:45:26 +0200

    base Routing

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProduitDetailComponent } from './components/produit-detail/produit-detail.component';
+import { ProduitsComponent } from './components/produits/produits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProduitsComponent
+  },
+  {
+    path: 'products/:id',
+    component: ProduitDetailComponent
+  },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'home', redirectTo: 'products', pathMatch: 'full' },
+  {
+    path: '**',
+    redirectTo: ''
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
